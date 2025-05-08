cb6c728139031b854482c64cda8df5bde89a1b14
ian <390859202@qq.com>
2025-05-08 10:18:42 +0800

    ok

--- tasks/deploy-fund.js
+++ tasks/deploy-fund.js
@@ -6,20 +6,20 @@ task("deploy-fund", "部署合约").setAction(async(taskArgs, hre) => {
     const [owner] = await ethers.getSigners()
     console.log(`部署合约owner账户:${owner.address}`)
 
-    const fundMe = await ethers.getContractFactory("Fund").then(factory => factory.connect(owner).deploy(1000));
-    await fundMe.waitForDeployment();
-    console.log(`合约部署成功,合约地址: ${fundMe.target}`);
+    const fund = await ethers.getContractFactory("Fund").then(factory => factory.connect(owner).deploy(1000));
+    await fund.waitForDeployment();
+    console.log(`合约部署成功,合约地址: ${fund.target}`);
 
     if (hre.network.config.chainId == 11155111 && process.env.ETHERSCAN_KEY){
         console.log(`等待3个区块确认`)
-        await fundMe.deploymentTransaction().wait(3)
-        await verifyFundMe(fundMe.target,[1000])
+        await fund.deploymentTransaction().wait(3)
+        await verifyfund(fund.target,[1000])
     } else {
         console.log("verification skipped..")
     }
 } )
 
-async function verifyFundMe(fundAddress, args) {
+async function verifyfund(fundAddress, args) {
     await hre.run("verify:verify", {
         address: fundAddress,
         constructorArguments: args,
